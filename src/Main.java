9a20a6d2f182db1ef49801fe3331ed302a657a22
jenn-martinez <martinezvargasandre@gmail.com>
2024-08-27 14:22:22 -0500

    Nueva rama

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
- SharickDoncel
-        System.out.println("Sharick Doncel");
-        System.out.println("Zipaquira, potosi");
-        System.out.println("19");
-
-
-
+        //Jennyfer Andrea Martinez Vargas
+        //Zipaquirá
+        //18 años
     }
 }
