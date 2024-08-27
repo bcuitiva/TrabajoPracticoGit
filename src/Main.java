7526f2b420d14a96ec4b2c71642836afe0b3a07b
Andres Corredor <corredor011andres@gmail.com>
2024-08-27 13:49:57 -0500

    nueva rama

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Brayan Cuitiva");
+        System.out.println("Andres Corredor");
+        System.out.println("Sesquile");
+        System.out.println("19 años");
     }
 }
