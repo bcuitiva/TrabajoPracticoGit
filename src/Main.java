a5193aa276304d6b9deb7373acc304afde2a3705
Elkin Almonacid <elkinyamith13@gmail.com>
2024-08-27 13:41:21 -0500

    Informacion personal añadida

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Brayan Cuitiva");
+        System.out.println("Elkin Yamith Almonacid López");
+        System.out.println("Sopó, Cundinamarca");
+        System.out.println("18 años");
     }
 }
