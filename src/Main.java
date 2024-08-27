db02dac8cb9c80034c07f53d61c9cb25208e0b4b
Gerlopak <elweydelpanxd.com>
2024-08-27 13:42:22 -0500

    se agrega nombre

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Brayan Cuitiva");
+        System.out.println("Deivid Andres Real Franco");
+        System.out.println("Zipaquira");
+        System.out.println("19");
     }
 }
