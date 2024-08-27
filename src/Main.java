a35aff0220fe6f5d440b2c48bc5a3f127e3e70f6
ESTUDIANTE <Ju4ng4rzon2002@gmail.com>
2024-08-27 13:47:58 -0500

    Se puso nombre, ciodad y edad

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Brayan Cuitiva");
+        System.out.println("Juan Garzon");
+        System.out.println("Chia");
+        System.out.println("22 años");
+
+
     }
 }
