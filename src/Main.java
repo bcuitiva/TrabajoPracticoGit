049f00d9068e720925168a6e89addb19a55cdf54
Brayan Mendez <almoconstrucciones>
2024-08-27 13:43:56 -0500

    Se agrego nombre, ciudad y edad

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Brayan Cuitiva");
+        System.out.println("Brayan Mendez");
+        System.out.println("Sopó, Cundinamarca");
+        System.out.println("18 años");
 
     }
 }
